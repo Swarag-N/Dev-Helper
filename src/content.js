5e11ec68452c7d8b64bbba1bb16a5a796c917c36
whois-this <46181010+whois-this@users.noreply.github.com>
2020-11-10 12:30:30 +0530

    Add Icon &  Sanitize Code

--- src/content.js
+++ src/content.js
@@ -1,11 +1,16 @@
 "use strict";
 console.log("Using Dev-Helper");
 
+// Using const
 const bodyElement = document.getElementsByTagName("body");
 let formData = {};
 let borderProperties = `${formData.size}px ${formData.color} ${formData.bStyle}`;
 // let borderProperties = "1px dashed #0000FF"
 
+/**
+ *
+ * @param {*} event eventHandler
+ */
 function devHelper(event) {
   // highlight the mouseover target
   event.target.style.border = borderProperties;
@@ -16,6 +21,12 @@ function devHelper(event) {
   }, 500);
 }
 
+/**
+ * @function DevHelperController Handles the commands come from popup
+ * @param {*} commands contains the border properties user need
+ * @param {*} sender
+ * @param {*} sendResponse
+ */
 function DevHelperController(commands, sender, sendResponse) {
   console.log(commands);
   if (commands.onoffswitch) {
